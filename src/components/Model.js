525256b3d467ec78a5824b755601c2f3b78a88d4
initmrd <initmrd@gmail.com>
2016-11-17 08:43:06 +0800

    模板component

---
+++ src/components/Model.js
@@ -0,0 +1,25 @@
+/**
+ * Created by mrd on 16/11/16.
+ */
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+import {StyleSheet, Navigator, Platform, View, Text, TouchableOpacity, Modal, TextInput} from 'react-native';
+
+class Model extends Component {
+    render() {
+
+    }
+}
+
+var styles = StyleSheet.create({
+
+})
+
+function select(store) {
+    return {
+
+    }
+}
+
+export default connect(select)(Model);
