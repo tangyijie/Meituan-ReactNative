23dc2b66ae487e0cf5552d3db89d93177d971536
initmrd <initmrd@gmail.com>
2016-11-28 16:06:46 +0800

    Android添加对百度地图的支持

--- android/app/src/main/java/com/meituan/MainApplication.java
+++ android/app/src/main/java/com/meituan/MainApplication.java
@@ -27,7 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new BaiduMapPackage(),
+            new BaiduMapPackage(getApplicationContext()),
             new VectorIconsPackage(),
             new AppVersionPackage()
       );
